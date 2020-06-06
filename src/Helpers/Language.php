029a834b4b495674431eb5ac586cb8d3c2b4fdea
Filip Jankoski <filip@agmedia.hr>
2020-06-06 21:46:28 +0200

    Language, Settings.

---
+++ src/Helpers/Language.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Agmedia\Helpers;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+    
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+    
+    /**
+     * @var string
+     */
+    protected $table = 'language';
+    
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'language_id';
+    
+    /**
+     * @var array
+     */
+    protected $guarded = [
+        'language_id'
+    ];
+}
