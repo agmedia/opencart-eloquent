8d393b5e641cb79ba4f8f103ac04c08d92ef1935
Filip Jankoski <filip@agmedia.hr>
2020-11-18 19:44:40 +0100

    Product model updated (activate, deactivate functions, description relations).

--- src/Models/Product/Product.php
+++ src/Models/Product/Product.php
@@ -32,6 +32,7 @@ class Product extends Model
         'product_id'
     ];
     
+    
     /**
      * Relations methods
      */
@@ -42,6 +43,26 @@ class Product extends Model
     public function options()
     {
         return $this->hasMany(ProductOption::class, 'product_id', 'product_id')->with('value');
+    }
+    
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function descriptions()
+    {
+        return $this->hasMany(ProductDescription::class, 'product_id', 'product_id');
+    }
+    
+    
+    /**
+     * @param $language_id
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function description($language_id)
+    {
+        return $this->hasOne(ProductDescription::class, 'product_id', 'product_id')->where('language_id', $language_id);
     }
     
     /**
@@ -56,6 +77,39 @@ class Product extends Model
     public function scopeSkus($query)
     {
         return ProductOption::where('product_id', $this->product_id)->pluck('sku');
+    }
+    
+    /**
+     * Custom methods
+     */
+    
+    /**
+     * @param $products - Should be array or integer.
+     *
+     * @return mixed
+     */
+    public function activate($products)
+    {
+        if (is_array($products)) {
+            return $this->whereIn('product_id', $products)->update(['status' => 1]);
+        }
+        
+        return $this->where('product_id', $products)->update(['status' => 1]);
+    }
+    
+    
+    /**
+     * @param $products - Should be array or integer.
+     *
+     * @return mixed
+     */
+    public function deactivate($products)
+    {
+        if (is_array($products)) {
+            return $this->whereIn('product_id', $products)->update(['status' => 0]);
+        }
+        
+        return $this->where('product_id', $products)->update(['status' => 0]);
     }
     
 }
